dba42d7cdd8e9df00c6c4d63679297e18dc33d1f
Ketan Ramteke <Ketantoo@gmail.com>
2022-12-19 23:59:38 +0530

    News Widget is done

--- news_app_flutter/lib/widgets/news_headline_widget.dart
+++ news_app_flutter/lib/widgets/news_headline_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_flutter/utils.dart';
 
 class NewsHeadlineWidget extends StatelessWidget {
   const NewsHeadlineWidget({Key? key}) : super(key: key);
@@ -21,6 +22,92 @@ class NewsHeadlineWidget extends StatelessWidget {
               height: 150,
               fit: BoxFit.fill,
             ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(right: 5, left: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "kajbdsbasjkavsckja",
+                      style: textStyle(
+                        16,
+                        Colors.black,
+                        FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Jonh Doe",
+                          style: textStyle(
+                            14,
+                            Colors.grey,
+                            FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "12-12-2022",
+                          style: textStyle(
+                            14,
+                            Colors.grey,
+                            FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Learning a little each day adds up. Research shows that students who make learning a habit are more likely to reach their goals. Set time aside to learn and get reminders using your learning scheduler.",
+                        overflow: TextOverflow.clip,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.zero,
+                          icon: Icon(
+                            Icons.play_arrow,
+                            size: 43,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.zero,
+                          icon: Icon(
+                            Icons.stop,
+                            size: 43,
+                            color: Colors.black,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.yellow,
+                          ),
+                          child: Text(
+                            "View",
+                            style: textStyle(15, Colors.black, FontWeight.w600),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
